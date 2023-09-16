ddd883f0720d9fc0e1990e63d977fe4cfa7f120a
Varun Maheshwari <93255621+Varunma82@users.noreply.github.com>
2023-09-16 14:51:22 +0530

    Create addFirstaddLastaddMiddle.java

---
+++ addFirstaddLastaddMiddle.java
@@ -0,0 +1,73 @@
+public class add{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    public void addLast(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+        }
+        tail.next=newNode;
+        newNode=tail;
+    }
+    public void print(){
+        if(head==null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public void addMiddle(int idx,int data){
+        Node newNode=new Node(data);
+        Node temp=head;
+        int i=0;
+        while(i<idx-1){
+            temp=temp.next;
+            i++;
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+    }
+    public static void main(String[] args) {
+        add ll=new add();
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addFirst(2);
+        ll.print();
+        ll.addFirst(3);
+        ll.print();
+        ll.addLast(1);
+        ll.print();
+        ll.addLast(2);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addMiddle(2, 9);
+        ll.print();
+    }
+}
